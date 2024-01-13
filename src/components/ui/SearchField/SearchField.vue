cbe852912995f537ad7b4df38ea4e807716112fe
Roman Trashutin <tttrashutin@gmail.com>
2024-01-13 23:56:18 +0200

    feat: add SearchField component

---
+++ src/components/ui/SearchField/SearchField.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+
+interface Props {
+  name: string
+}
+const props = defineProps<Props>()
+</script>
+<template>
+  <div class="Container flex justify-between items-center w-full">
+    <input
+      class="font-bold text-xl leading-6 w-full tracking-tight border-none outline-none"
+      type="text"
+      :name="props.name"
+    />
+    <Icon class="Icon" icon="ic:outline-search" />
+  </div>
+</template>
+<style lang="css" scoped>
+.Container {
+  @apply bg-secondary-container;
+  border-radius: 28px;
+  padding: 15px 15px 15px 20px;
+}
+input {
+  font-family: 'Montserrat', sans-serif;
+  background-color: transparent;
+  @apply text-contrast;
+}
+.Icon {
+  @apply text-emoji;
+  height: 24px;
+  width: 24px;
+}
+</style>
