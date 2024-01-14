9d376624aefcb5e0ca4b11a13ba165e6ada60faa
Roman Trashutin <tttrashutin@gmail.com>
2024-01-14 20:57:33 +0200

    fix: fix erros in field components

--- src/components/ui/SearchField/SearchField.vue
+++ src/components/ui/SearchField/SearchField.vue
@@ -1,35 +1,35 @@
 <script lang="ts" setup>
+import { useVModel } from '@vueuse/core'
 import { Icon } from '@iconify/vue'
 
 interface Props {
   name: string
+  modelValue: string
 }
 const props = defineProps<Props>()
+
+const emit = defineEmits(['update:modelValue'])
+const model = useVModel(props, 'modelValue', emit)
 </script>
 <template>
-  <div class="Container flex justify-between items-center w-full">
+  <div class="Container flex justify-between items-center w-full p-[15px_15px_15px_20px]">
     <input
-      class="font-bold text-xl leading-6 w-full tracking-tight border-none outline-none"
+      class="InputField font-bold text-xl leading-6 w-full tracking-tight border-none outline-none bg-transparent"
       type="text"
       :name="props.name"
+      v-model="model"
     />
-    <Icon class="Icon" icon="ic:outline-search" />
+    <Icon class="Icon w-6 h-6" icon="ic:outline-search" />
   </div>
 </template>
 <style lang="css" scoped>
 .Container {
-  @apply bg-secondary-container;
-  border-radius: 28px;
-  padding: 15px 15px 15px 20px;
+  @apply bg-secondary-container rounded-3xl;
 }
-input {
-  font-family: 'Montserrat', sans-serif;
-  background-color: transparent;
-  @apply text-contrast;
+.InputField {
+  @apply text-contrast font-montserrat;
 }
 .Icon {
   @apply text-emoji;
-  height: 24px;
-  width: 24px;
 }
 </style>
--- src/components/ui/TextField/TextField.vue
+++ src/components/ui/TextField/TextField.vue
@@ -1,55 +1,51 @@
 <script lang="ts" setup>
+import { useVModel } from '@vueuse/core'
 type TextFieldType = 'email' | 'text' | 'password' | 'search'
 
 interface TextFieldProps {
   id: string
   placeholder: string
   type: TextFieldType
+  modelValue: string
   error?: string | null
 }
-
 const props = defineProps<TextFieldProps>()
+
+const emit = defineEmits(['update:modelValue'])
+const model = useVModel(props, 'modelValue', emit)
 </script>
 
 <template>
-  <div class="Container">
-    <input :type="props.type" :id="props.id" :placeholder="props.placeholder" />
-    <label class="Placeholder" :id="props.id" :for="props.id">
-      <div class="Text">{{ props.placeholder }}</div>
+  <div class="flex flex-col relative">
+    <input
+      class="InputField flex justify-start items-center w-auto"
+      :type="props.type"
+      :id="props.id"
+      :placeholder="props.placeholder"
+      v-model="model"
+    />
+    <label
+      class="Placeholder flex items-center absolute top-0 bottom-0 left-0 right-0 bg-transparent pointer-events-none ml-2"
+      :id="props.id"
+      :for="props.id"
+    >
+      <div class="Text bg-transparent text-xl leading-6 font-bold">
+        {{ props.placeholder }}
+      </div>
     </label>
-    <div class="ErrorMessage" v-if="props.error !== null">{{ error }}</div>
+    <div class="ErrorMessage text-xs font-light" v-if="props.error !== null">{{ error }}</div>
   </div>
 </template>
 
-<style lang="css" scoped>
-.Container {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-}
-
+<style lang="scss" scoped>
 .Placeholder {
-  display: flex;
-  align-items: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   border: 3px solid transparent;
-  background-color: transparent;
-  pointer-events: none;
-  margin-left: 8px;
 }
 
 .Text {
-  font-size: 20px;
-  font-weight: 700;
-  font-family: 'Montserrat', sans-serif;
+  @apply text-calendar font-montserrat;
   padding: 0 0.5rem;
-  background-color: transparent;
   transform: translate(0);
-  @apply text-calendar;
   transition:
     transform 0.15s ease-out,
     font-size 0.15s ease-out,
@@ -57,54 +53,46 @@ const props = defineProps<TextFieldProps>()
     color 0.15s ease-out;
 }
 
-input {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  width: auto;
+.InputField {
+  @apply bg-app-bg text-contrast border-navbar-chip font-montserrat;
   min-width: 210px;
   height: 56px;
-  @apply bg-app-bg;
-  font-size: 20px;
-  font-weight: 700;
-  font-family: 'Montserrat', sans-serif;
-  @apply text-contrast;
-  line-height: 24px;
   border-radius: 4px;
   border-width: 2px;
-  boder-style: solid;
-  @apply border-navbar-chip;
+  border-style: solid;
 
   padding: 8px 0px 8px 16px;
   box-sizing: border-box;
+
+  &:hover,
+  &:focus {
+    outline: none;
+    border-width: 2px;
+    border-style: solid;
+    @apply border-outline;
+  }
+
+  &:focus + .Placeholder .Text {
+    @apply bg-app-bg text-outline;
+    font-size: 1.1rem;
+    transform: translate(0, -170%);
+  }
+
+  &:focus + .Placeholder .Text {
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    @apply text-contrast font-roboto;
+  }
+
+  &:not(&:focus):not(&[value='']) + .Placeholder .Text {
+    overflow: hidden;
+    font-size: 0;
+    color: transparent;
+  }
 }
 
-input:hover,
-input:focus {
-  outline: none;
-  border-width: 2px;
-  boder-style: solid;
-  @apply border-outline;
-}
-
-input:focus + .Placeholder .Text {
-  @apply bg-app-bg;
-  font-size: 1.1rem;
-  @apply text-outline;
-  transform: translate(0, -170%);
-}
-
-input:focus + .Placeholder .Text {
-  font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  font-size: 12px;
-  line-height: 16px;
-  @apply text-contrast;
-}
-
-input:not(input:focus):not(input[value='']) + .Placeholder .Text {
-  overflow: hidden;
-  font-size: 0;
-  color: transparent;
+.ErrorMessage {
+  @apply font-roboto text-error-bg;
 }
 </style>
