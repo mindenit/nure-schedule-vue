84a5006f30c424290f7abc63619c24f3e079e7f9
OneLiL05 <kirillofficial0507@gmail.com>
2024-01-15 22:52:21 +0200

    feat: add Tabs component

---
+++ src/components/ui/Tabs/TabsContent.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { TabsContent, useForwardProps, type TabsContentProps } from 'radix-vue'
+
+const props = defineProps<TabsContentProps>()
+
+const forwarded = useForwardProps(props)
+</script>
+<template>
+  <TabsContent v-bind="forwarded">
+    <slot></slot>
+  </TabsContent>
+</template>
+<style lang="css" scoped></style>
---
+++ src/components/ui/Tabs/TabsRoot.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { TabsRoot, type TabsRootProps, type TabsRootEmits, useForwardPropsEmits } from 'radix-vue'
+
+const props = defineProps<TabsRootProps>()
+const emits = defineEmits<TabsRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+<template>
+  <TabsRoot v-bind="forwarded">
+    <slot></slot>
+  </TabsRoot>
+</template>
