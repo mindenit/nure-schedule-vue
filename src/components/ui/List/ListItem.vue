5478bdf0e3eea5dfc3cf815c7c0e409432b46d7f
OneLiL05 <kirillofficial0507@gmail.com>
2024-01-16 19:50:39 +0200

    feat: add List component

---
+++ src/components/ui/List/ListItem.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import {
+  useForwardPropsEmits,
+  useId,
+  type CheckboxRootEmits,
+  type CheckboxRootProps
+} from 'radix-vue'
+import { Checkbox } from '../Checkbox'
+
+const id = useId()
+const props = defineProps<CheckboxRootProps>()
+const emits = defineEmits<CheckboxRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+<template>
+  <label class="ListItem" :for="id">
+    <slot></slot>
+    <Checkbox v-bind="forwarded" :id="id" />
+  </label>
+</template>
+<style lang="scss" scoped>
+.ListItem {
+  @apply flex select-none flex-row items-center justify-between w-full h-12 font-roboto text-sm leading-6 not-italic text-contrast tracking-tight px-4 rounded-lg box-border;
+
+  &:hover,
+  &:focus-within {
+    @apply bg-surface-container;
+  }
+}
+</style>
---
+++ src/components/ui/List/ListRoot.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+<template>
+  <section class="ListRoot">
+    <slot></slot>
+  </section>
+</template>
+<style lang="css" scoped>
+.ListRoot {
+  @apply flex flex-col items-start justify-start w-full;
+}
+</style>
