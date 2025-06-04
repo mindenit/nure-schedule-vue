233c36a0a7338a084ae72236481f19989114c47c
perkinson1251 <tttrashutin@gmail.com>
2025-06-04 13:50:10 +0300

    feat: add warning bar

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -8,6 +8,9 @@ defineProps<{ title: string }>()
   <main class="box-border flex h-dvh flex-col">
     <MobileHeader title="Розклад" />
     <Navbar :title="title" />
+    <div class="flex w-full items-center justify-center bg-[#fdc700] px-2 text-black">
+      На даний момент розклад не доступний. Будь ласка, спробуйте пізніше.
+    </div>
     <slot></slot>
   </main>
 </template>
